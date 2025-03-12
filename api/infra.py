723607481b2f1bbc37073169161be33143c7279e
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2025-03-12 16:13:08 +0600

    Remove method `dispatch_request()` from `APIView`

--- api/infra.py
+++ api/infra.py
@@ -2,9 +2,9 @@ from flask.views import MethodView
 
 
 class APIView(MethodView):
+    """
+    Generic view for APIs
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-    def dispatch_request(self, *args, **kwargs):
-        return super().dispatch_request(*args, **kwargs)
